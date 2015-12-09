5ec39fb997c14f79ff1ab4dc905ff07c9e6127c8
Pavel Gordeev <mitridat2000@gmail.com>
2015-12-09 14:05:34 +0300

    =first commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+
+defined('APPLICATION_PATH')
+    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/'));
+
+
+include 'BrowserAdapter.php';
+include 'BrowserCurl.php';
+include 'simplehtmldom/simple_html_dom.php';
+
+$br = new Ap_Browser_BrowserCurl();
+$url = 'http://193.27.243.130/cgi-bin/irbis64r_91/cgiirbis_64.exe';
+$params = array(
+    'X_S21P03' => 'K=',
+    'I21DBN' => 'IBIS',
+    'P21DBN' => 'IBIS',
+    'X_S21STR' => 'Пушкин',
+    'X_S21P01' => '4',
+    'X_S21P02' => '1',
+    'X_S21LOG' => '1',
+    'S21COLORTERMS' => '1',
+    'S21FMT' => 'infow_wh',
+    'S21STN' => '1',
+    'S21CNR' => '20',
+    'S21REF' => '3',
+    'C21COM' => 'S',
+    'C21COM1' => 'Поиск'
+);
+$br->post($url, $params);
+
+$html = str_get_html($br->getResponseText());
+
+// find all link
+foreach($html->find('.advanced tr td[width="95%"]') as $e) {
+    $str = str_replace('<br>', "\n", $e->innertext);
+    echo $str . "\n\n\n\n\n\n\n\n\n\n\n\n";
+}
